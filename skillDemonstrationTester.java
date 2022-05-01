2356459d8f0820a732262b4ca1d9dafc1c18de9d
molmedo3 <molmedo@ucsd.edu>
2022-04-30 20:05:31 -0700

    fix test case

--- skillDemonstrationTester.java
+++ skillDemonstrationTester.java
@@ -1,14 +1,14 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-// javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar skill-demo-test.java
-// java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore skill-demo-test
+// javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar skillDemonstrationTester.java
+// java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore skillDemonstrationTester
 
 public class skillDemonstrationTester {
     @Test
     public void maxIntTest(){
         int[] arr = {1,4,6,0};
         int maxInt= skillDemonstration.maxInt(arr);
-        assertEquals(0, maxInt);
+        assertEquals(6, maxInt);
 
     }
 }
